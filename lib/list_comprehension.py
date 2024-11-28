386835b443d815965e2ea6af01971f82203024f4
ianimbuga <imbugaian20@gmail.com>
2024-11-28 10:46:47 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even = [num for num in num_list if num % 2 ==0]
+    return even
     pass
 
 def make_exclamation(sentence_list):
+    modified = [objects + "!" for objects in sentence_list]
+    return modified
     pass
